e755b40bda3c52bad310be332a277f0cb9805321
Weptzel <wmorar00@estudiantes.unileon.es>
2025-04-08 22:27:22 +0200

    faltan cosas circular

--- ED_Pract3_2025/src/ule/ed/doubleList/DoubleLinkedCircularList.java
+++ ED_Pract3_2025/src/ule/ed/doubleList/DoubleLinkedCircularList.java
@@ -170,43 +170,127 @@ public class DoubleLinkedCircularList<T> implements IDoubleList<T> {
 
 	@Override
 	public boolean isEmpty() {
-		return false;
 		// TODO Auto-generated method stub
+		/* Vacio si cab apunta a sí mismo */
+		return cab.next == cab && cab.prev == cab;
 		}
 
 	@Override
 	public void clear() {
 		// TODO Auto-generated method stub
+		cab.next = cab;
+		cab.prev = cab;
 	}
 
 	@Override
 	public void addFirst(T elem, int n) {
 		// TODO Auto-generated method stub
-		
+		if(elem == null){
+			throw new NullPointerException();
+		}
+		if(n <= 0){
+			throw new IllegalArgumentException();
+		} 
+
+		/* Buscar elemento */
+		DoubleNode<T> current = cab.next;
+		while(current != cab){
+			/* si ya está en la lista aumentar count */
+			if(current.elem.equals(elem)){
+				current.count += n;
+				return;
+			}
+			current = current.next;
+		}
+		DoubleNode<T> nuevoNodo = new DoubleNode<T>(elem, n);
+		/* Insertar al principio */
+		nuevoNodo.next = cab.next;
+		nuevoNodo.prev = cab;
+		cab.next.prev = nuevoNodo;
+		cab.next = nuevoNodo;
 	}
 
 	@Override
 	public void addLast(T elem, int n) {
 		// TODO Auto-generated method stub
+		if(elem == null){
+			throw new NullPointerException();
+		}
+		if(n <= 0){
+			throw new IllegalArgumentException();
+		}
+		DoubleNode<T> current = cab.next;
+		while(current.next != cab){
+			if(current.elem.equals(elem)){
+				current.count += n;
+				return;
+			}
+			current = current.next;
+		}
+		DoubleNode<T> nuevoNodo = new DoubleNode<T>(elem, n);
+		/* Insertar al final */
+		nuevoNodo.next = cab;
+		nuevoNodo.prev = cab.prev;
+		cab.prev.next = nuevoNodo;
+		cab.prev = nuevoNodo;
 		
 	}
 
 	@Override
 	public int removeFirst(int num) throws EmptyCollectionException {
 		// TODO Auto-generated method stub
-		return 0;
+		if(isEmpty()){
+			throw new EmptyCollectionException("Lista vacía");
+		}
+		if (num <= 0){
+			throw new IllegalArgumentException();
+		}
+
+		DoubleNode<T> primero = cab.next;
+		if(primero.count > num){
+			primero.count -= num;
+			return num;
+		} 
+
+		int borradas = primero.count;
+		cab.next = primero.next; // Actualizar el puntero `next` del nodo cabecera
+		primero.next.prev = cab; // Actualizar el puntero `prev` del nuevo primer nodo
+		return borradas;
 	}
 
 	@Override
 	public T removeAllFirst() throws EmptyCollectionException {
 		// TODO Auto-generated method stub
-		return null;
+		if(isEmpty()){
+			throw new EmptyCollectionException("Lista vacía");
+		}
+		DoubleNode<T> primero = cab.next;
+		T borrado = primero.elem;
+
+		/* actualizar referencias */
+		cab.next = primero.next; 
+		primero.next.prev = cab;
+		return borrado;
 	}
 
 	@Override
 	public int removeLast(int num) throws EmptyCollectionException {
 		// TODO Auto-generated method stub
-		return 0;
+		if(isEmpty()){
+			throw new EmptyCollectionException("Lista vacía");
+		}
+		if( num <= 0){
+			throw new IllegalArgumentException();
+		}
+		DoubleNode<T> ultimo = cab.prev;
+		if(ultimo.count > num){
+			ultimo.count -= num;
+			return num;
+		}
+		int borradas = ultimo.count;
+		ultimo.prev.next = cab; // Actualizar el puntero `next` del nodo cabecera
+		cab.prev = ultimo.prev; // Actualizar el puntero `prev` del nodo cabecera
+		return borradas;
 	}
 
 	@Override
@@ -224,25 +308,49 @@ public class DoubleLinkedCircularList<T> implements IDoubleList<T> {
 	@Override
 	public T getElemPos(int position) {
 		// TODO Auto-generated method stub
+
 		return null;
 	}
 
 	@Override
 	public boolean contains(T elem) {
 		// TODO Auto-generated method stub
+		if(elem == null){
+			throw new NullPointerException();
+		}
+		DoubleNode<T> current = cab.next;
+		while(current != cab){
+			if(current.elem.equals(elem)){
+				return true;
+			}
+			current = current.next;
+		}
 		return false;
 	}
 
 	@Override
 	public int size() {
 		// TODO Auto-generated method stub
-		return 0;
+		int count = 0;
+		DoubleNode<T> current = cab.next;
+		while(current != cab){
+			count++;
+			current = current.next;
+		}
+		return count;
 	}
 
 	@Override
 	public int countInstances() {
 		// TODO Auto-generated method stub
-		return 0;
+		if(isEmpty()){return 0;}
+		int count = 0;
+		DoubleNode<T> current = cab.next;
+		while(current != cab){
+			count += current.count;
+			current = current.next;
+		}
+		return count;
 	}
 
 	@Override
